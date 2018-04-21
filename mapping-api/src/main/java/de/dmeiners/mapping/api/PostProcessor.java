61a765f38990b6487b86f375aa78d1879d147f22
Daniel Meiners <mail@dmeiners.de>
2018-04-21 16:41:20 +0200

    [docs] Adapt hint in JavaDoc to changed implementation

--- mapping-api/src/main/java/de/dmeiners/mapping/api/PostProcessor.java
+++ mapping-api/src/main/java/de/dmeiners/mapping/api/PostProcessor.java
@@ -7,7 +7,7 @@ public interface PostProcessor {
     /**
      * Resolves a given script name to a script text and compiles it.
      * The script name will be resolved with this instance's {@link ScriptNameResolver}.
-     * Use {@link PostProcessorFactory#create(ScriptNameResolver)} to specify a resolver at creation time.
+     * Use {@link PostProcessorFactory#builder()} to specify a resolver at creation time.
      *
      * @throws ParseException if the script text can not be parsed
      */
