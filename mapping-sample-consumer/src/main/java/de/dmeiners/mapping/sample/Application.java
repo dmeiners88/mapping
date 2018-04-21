a75e2a7934fcf5ab809568b32ec96d56e6b7b22d
Daniel Meiners <mail@dmeiners.de>
2018-04-21 16:50:10 +0200

    [feat] Update sample consumer app with latest features

--- mapping-sample-consumer/src/main/java/de/dmeiners/mapping/sample/Application.java
+++ mapping-sample-consumer/src/main/java/de/dmeiners/mapping/sample/Application.java
@@ -3,6 +3,7 @@ package de.dmeiners.mapping.sample;
 import de.dmeiners.mapping.api.PostProcessor;
 import de.dmeiners.mapping.api.PostProcessorFactory;
 import de.dmeiners.mapping.api.Script;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,12 +21,13 @@ public class Application {
 
         simpleUsage();
         contextUsage();
+        exposeLibrary();
     }
 
     private static void simpleUsage() {
 
         // tag::simpleUsage[]
-        PostProcessor processor = PostProcessorFactory.create();
+        PostProcessor processor = PostProcessorFactory.builder().build();
 
         String someObject = "Hello";
 
@@ -40,7 +42,7 @@ public class Application {
     private static void contextUsage() {
 
         // tag::contextUsage[]
-        PostProcessor processor = PostProcessorFactory.create();
+        PostProcessor processor = PostProcessorFactory.builder().build();
 
         Map<String, Object> user = new HashMap<>();
         user.put("firstName", "John");
@@ -57,6 +59,33 @@ public class Application {
 
         String result = script.execute(someObject, context);
         // end::contextUsage[]
+
+        logger.debug(result);
+    }
+
+    private static void exposeLibrary() {
+
+        // tag::exposeLibrary[]
+        PostProcessor processor = PostProcessorFactory.builder()
+            .extension("StringUtils", StringUtils.class)
+            .extension("Tools", Tools.class)
+            .build();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("firstName", "John");
+        user.put("lastName", "Doe");
+
+        Map<String, Object> context = new HashMap<>();
+        context.put("user", user);
+
+        String someObject = "Hello";
+
+        String scriptText = "if (StringUtils.isNotBlank(user.firstName)) { target = 'First name is not blank and reversed ' + Tools.reverse(user.firstName); }";
+
+        Script script = processor.compileInline(scriptText);
+
+        String result = script.execute(someObject, context);
+        // end::exposeLibrary[]
 
         logger.debug(result);
     }
---
+++ mapping-sample-consumer/src/main/java/de/dmeiners/mapping/sample/Tools.java
@@ -0,0 +1,14 @@
+package de.dmeiners.mapping.sample;
+
+import org.apache.commons.lang3.StringUtils;
+
+public final class Tools {
+
+    private Tools() {
+    }
+
+    public static String reverse(String input) {
+
+        return StringUtils.reverse(input);
+    }
+}
